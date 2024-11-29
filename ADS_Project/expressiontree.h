6794bc469c88f692afc249ec6fc552f288de0f64
Ahmed-Soliman1 <161841353+Ahmed-Soliman1@users.noreply.github.com>
2024-11-29 17:43:27 +0200

    Overloaded isOperator Function

--- ADS_Project/expressiontree.h
+++ ADS_Project/expressiontree.h
@@ -19,13 +19,13 @@ private:
     void PreOrderTraversal();
     void PostOrderTraversal();//Helper Functions to be included in the Conversions
     bool isOperator(QChar ch);
+    bool isStringOperator(QString c);
     int precedence(QChar op);
 
 public:
     ExpressionTree();
     void buildfromPostfix(const QString & postfix); // building expression tree from postfix expression
-    TreeNode* buildfromPrefix(const QString & prefix, int & index);
-    TreeNode* Helper_buildfromPrefix(const QString & prefix);
+    void buildfromPrefix(const QString & prefix);
     void buildfromInfix(const QString & infix);
     QString ToInfix(TreeNode* Root);
     QString ToPostfix(TreeNode* Root);
